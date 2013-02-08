783126614fa7b62e6cdef381bf921e34c2955379
Christopher Krause <ckrause@knights.ucf.edu>
2013-02-08 14:50:17 -0500

    Created local repository - Chris

---
+++ Assets/Assignment1/Scripts/Floor.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Floor : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () 
+	{
+		renderer.material.color = Color.magenta;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
